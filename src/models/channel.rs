6196ede51c127075595093813dd1d7420d578a66
AbhinavRobinson <abhinavrobinson@gmail.com>
2022-10-08 17:56:25 +0530

    📒 Add lists for messages and users

--- src/models/channel.rs
+++ src/models/channel.rs
@@ -1,13 +1,23 @@
 use std::sync::atomic::{AtomicUsize, Ordering};
 
+use super::{message::Message, user::User};
+
+#[derive(Debug)]
 pub struct Channel {
     pub id: usize,
+    pub users: Vec<User>,
+    pub messages: Vec<Message>,
     id_manager: AtomicUsize,
 }
 
 impl Channel {
     pub fn new(id: usize, id_manager: AtomicUsize) -> Self {
-        Self { id, id_manager }
+        Self {
+            id,
+            users: vec![],
+            messages: vec![],
+            id_manager,
+        }
     }
     pub fn get_id(&self) -> usize {
         self.id_manager.fetch_add(1, Ordering::Relaxed)
